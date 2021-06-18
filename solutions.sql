f599fca2d6384dac3829aca97e025c0dad89f404
Kevin Spurk <kevin.lee.spurk@gmx.de>
2021-06-18 14:12:35 +0200

    solutions to lab

---
+++ solutions.sql
@@ -0,0 +1,22 @@
+use sakila;
+
+-- Exercise 3
+select title
+from film
+limit 30;
+
+-- Exercise 4
+select distinct name as language
+from language;
+
+-- Exercise 5.1
+select count(store_id)
+from store;
+
+-- Exercise 5.2
+select count(staff_id)
+from staff;
+
+-- Exercise 5.3
+select distinct first_name
+from staff;
